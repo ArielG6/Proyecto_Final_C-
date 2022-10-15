2ade9da445231d5fa1f24d2e21eb9c178c8a9a06
ArielG6 <82763802+ArielG6@users.noreply.github.com>
2022-10-15 20:21:12 -0300

    Versión final pre entrega

--- Proyecto_Final_C-/Clases/Venta.cs
+++ Proyecto_Final_C-/Clases/Venta.cs
@@ -1,12 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Proyecto_Final_C_.Clases
 {
-    internal class Venta
+    public class Venta
     {
+        public int Id { get; set; }
+        public string Comentarios { get; set; }
+        public int IdUsuario { get; set; }
+       
+        //Constructor por defecto
+        public Venta()
+        {
+            Id = 0;
+            Comentarios = String.Empty;
+            IdUsuario = 0;
+        }
+        //Constructor parametrizado
+        public Venta (int ID, string Comentarios, int IdUsuario)
+        {
+            this.Id = Id;
+            this.Comentarios = Comentarios;
+            this.IdUsuario = IdUsuario;
+        }
+        public List<Venta> TraerVentas(int IdUsuarioBuscar)
+        {
+            //Método que recibe un número de IdUsuario como parámetro y
+            //trae todas las ventas de la base asignados a este usuario en particular.
+
+            var listaVentas = new List<Venta>();
+
+            SqlConnectionStringBuilder connectionBuilder = new();
+            connectionBuilder.DataSource = "LAPTOP-JBSHHD62";
+            connectionBuilder.InitialCatalog = "SistemaGestion";
+            connectionBuilder.IntegratedSecurity = true;
+
+            var connectionString = connectionBuilder.ConnectionString;
+
+            using (SqlConnection connect = new SqlConnection(connectionString))
+            {
+                connect.Open();
+
+                SqlCommand cmd = connect.CreateCommand();
+                cmd.CommandText = "SELECT * FROM Venta WHERE IdUsuario = @idUsu";
+                var parametro = new SqlParameter("idUsu", System.Data.SqlDbType.Int);
+                parametro.Value = IdUsuarioBuscar;
+                cmd.Parameters.Add(parametro);
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Venta venta = new Venta();
+
+                        venta.Id = Convert.ToInt32(dr.GetValue(0));
+                        venta.Comentarios = dr.GetString(1).ToString();
+                        venta.IdUsuario = Convert.ToInt32(dr.GetValue(2));
+
+                        listaVentas.Add(venta);
+
+                    }
+                    dr.Close();
+                }
+
+                connect.Close();
+            }
+
+            return listaVentas;
+        }
     }
 }
--- Proyecto_Final_C-/Program.cs
+++ Proyecto_Final_C-/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Proyecto_Final_C_.Clases;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Pruebas de funcionamiento de los métodos
+        //Traer usuario
+        Usuario usuario = new Usuario();
+        usuario = usuario.TraerUsuario("eperez");
+        Console.WriteLine("Id: " + usuario.Id);
+        Console.WriteLine("Nombre: " + usuario.Nombre);
+        Console.WriteLine("Apellido: " + usuario.Apellido);
+        Console.WriteLine("Nombre de Usuario: " + usuario.NombreUsuario);
+        Console.WriteLine("Contraseña: " + usuario.Contraseña);
+        Console.WriteLine("Email: " + usuario.Mail);
+        //Traer producto
+        Producto producto = new Producto();
+        var listaProductos = new List<Producto>();
+        listaProductos = producto.TraerProducto(1);
+        foreach (Producto prod in listaProductos)
+        {
+            Console.WriteLine(prod.Descripciones);
+        }
+        //Traer productos vendidos
+        var listaProductosVendidos = new List<ProductoVendido>();
+        ProductoVendido productoVendido = new ProductoVendido();
+        listaProductosVendidos = productoVendido.TraerProductosVendidos(1);
+        foreach (ProductoVendido prod in listaProductosVendidos)
+        {
+            Console.WriteLine(prod.IdProducto);
+        }
+        //Traer ventas
+        Venta venta = new Venta();
+        var listaVentas = new List<Venta>();
+        listaVentas = venta.TraerVentas(1);
+        foreach (Venta vent in listaVentas)
+        {
+            Console.WriteLine(vent.Id);
+        }
+        //Inicio de sesión
+        usuario = usuario.InicioDeSesión("eperez","SoyErnestoPerez");   //Usuario y contraseña correctos
+        //usuario = usuario.InicioDeSesión("eperez","SoyErstoPerez"); //Contraseña incorrecta
+        Console.WriteLine("Id: " + usuario.Id);
+        Console.WriteLine("Nombre: " + usuario.Nombre);
+        Console.WriteLine("Apellido: " + usuario.Apellido);
+        Console.WriteLine("Nombre de Usuario: " + usuario.NombreUsuario);
+        Console.WriteLine("Contraseña: " + usuario.Contraseña);
+        Console.WriteLine("Email: " + usuario.Mail);
+    }
+
+}
